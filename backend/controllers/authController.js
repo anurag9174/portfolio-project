e56a0db319ccaf97259e4458cee9d1bc081d0b30
anu <anuraggupta08708@gmail.com>
2026-09-03 15:03:53 +0530

    Day 4 - Authorization

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,15 +64,16 @@ const loginUser = async (req, res) => {
         }
 
         const token = jwt.sign(
-            {
-                id: user._id,
-                email: user.email
-            },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: "1d"
-            }
-        );
+    {
+        id: user._id,
+        email: user.email,
+        role: user.role
+    },
+    process.env.JWT_SECRET,
+    {
+        expiresIn: "1d"
+    }
+);
 
         res.json({
             message: "Login successful",
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,9 +5,20 @@ const {
     loginUser
 } = require("../controllers/authController");
 
+const protect = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Protected route - Admin only
+router.get("/admin", protect, authorize("admin"), (req, res) => {
+    res.json({
+        message: "Welcome Admin!",
+        user: req.user
+    });
+});
 
 module.exports = router;
