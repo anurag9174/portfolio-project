5d19eb43b2074551a91e29ca4eef12b881534844
anu <anuraggupta08708@gmail.com>
2026-09-05 11:00:47 +0530

    Day 8 - State Management

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,20 @@
+import { useSelector } from "react-redux";
+
 function Dashboard() {
-    return <h1>Dashboard Page</h1>;
+
+    const auth = useSelector((state) => state.auth);
+
+    console.log("Redux Auth State:", auth);
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+
+            <p>User: {auth.user?.name}</p>
+            <p>Email: {auth.user?.email}</p>
+            <p>Authenticated: {auth.isAuthenticated ? "Yes" : "No"}</p>
+        </div>
+    );
 }
 
 export default Dashboard;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../services/api";
+import { useDispatch } from "react-redux";
+import { login } from "../store/authSlice";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -16,6 +21,13 @@ function Login() {
             });
 
             console.log("Login Response:", response.data);
+
+            dispatch(login({
+                user: response.data.user,
+                token: response.data.token
+            }));
+
+            navigate("/dashboard");
 
         } catch (error) {
             console.error(
